8888c5a4d45fe28a975c48107f7fdab4d840b289
SharonMutabonwa <mutaron37@gmail.com>
2022-09-16 20:56:51 +0000

    Using Puppet to create a file in /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Creating a file in /tmp using Puppet
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
